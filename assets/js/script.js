52e304e14c0089d41d20965a38b029e664c04043
Tell Aulin <tell@tellemontage.se>
2024-04-21 17:36:20 +0000

    Add constants for the modal

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// A test of the modal function with rules
+//Adding const and evenelisteners to the modal
+
+//Modal const
+
+const modal = document.querySelector("#modal")
+const openModalButton = document.querySelector("open-modal-btn")
+const closeModalButton = document.querySelector("close-modal-btn")
+const overlay = document.querySelector("overlay")
